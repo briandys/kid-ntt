1389999937fb396684e5ea8cd503359d89ae2bfa
Brian Dys <briansahagun@gmail.com>
2020-01-15 07:29:04 +0800

    Updates

--- functions.php
+++ functions.php
@@ -45,3 +45,25 @@ add_filter( 'ntt_entity_maker_tag_theme_name_filter', function() {
 add_filter( 'ntt_entity_maker_tag_theme_url_filter', function() {
     return $GLOBALS['ntt_child_theme_url'];
 } );
+
+function ntt_entry_header_content() {
+
+    ntt_entry_categories();
+    ntt_entry_breadcrumbs_nav();
+    ntt_entry_heading();
+    ntt_entry_actions();
+    
+    if ( ( ( is_singular() || is_home() || is_archive() ) && has_excerpt() ) || is_search() ) {
+        ntt_entry_summary_content();
+    }
+
+    ntt_entry_banner();
+    ntt_entry_primary_meta();
+    ntt_comments_actions_snippet();
+}
+
+function ntt_entry_primary_meta_content() {
+
+    ntt_entry_author();
+    ntt_entry_datetime();   
+}
