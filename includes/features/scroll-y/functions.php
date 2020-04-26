8d58d8a73bd1e8f9ec912eff6c7bf3663ab1656a
Brian Dys <briansahagun@gmail.com>
2020-04-26 23:51:56 +0800

    Updates

--- includes/features/scroll-y/functions.php
+++ includes/features/scroll-y/functions.php
@@ -5,17 +5,22 @@
  */
 
 function ntt__kid_ntt__features__info() {
-    
     $name = 'Scroll Y';
 
     $info = array(
         'name'      => $name,
         'slug'      => sanitize_title( $name ),
         'version'   => '0.0.1',
+        'prefix'    => 'ntt--kid-ntt--feature--',
     );
     
     return $info;
 }
+
+$feature_slug = ntt__kid_ntt__features__info()['slug'];
+$feature_prefix = ntt__kid_ntt__features__info()['prefix'];
+$feature_prefixed_name = $feature_prefix. $feature_slug;
+$feature_version = ntt__kid_ntt__features__info()['version'];
 
 $GLOBALS['ntt__gvar__kid_ntt__feature__scroll_y__prefixed_name'] = $GLOBALS['ntt__gvar__kid_ntt__feature__name_prefix']. $GLOBALS['ntt__gvar__kid_ntt__feature__scroll_y__name'];
 $GLOBALS['ntt__gvar__kid_ntt__feature__scroll_y__version'] = '1.0.0';
@@ -27,15 +32,17 @@ $GLOBALS['ntt__gvar__kid_ntt__feature__scroll_y__version'] = '1.0.0';
 function ntt__kid_ntt__feature__scroll_y__validation() {
     $post_meta = get_post_meta( get_the_ID(), 'ntt_feature', true );
     $theme_mod = get_theme_mod( 'ntt_settings_features' );
-
-    $ntt_f5e_array = array(
+    
+    $feature_array = array(
         ntt__kid_ntt__features__info()['slug'],
     );
 
-    if ( strpos_array( $post_meta, $ntt_f5e_array ) || strpos_array( $theme_mod, $ntt_f5e_array ) ) {
+    if ( strpos_array( $post_meta, $feature_array ) || strpos_array( $theme_mod, $feature_array ) ) {
         return true;
     }
 }
+
+echo ntt__kid_ntt__feature__scroll_y__validation();
 
 /**
  * NTT Feature Validation
@@ -44,11 +51,11 @@ function ntt__kid_ntt__feature__scroll_y__validation() {
 function ntt__kid_ntt__feature__scroll_y__snaps_settings_validation() {
     $snaps_feature_settings = join( ' ', ntt__kid_ntt__snaps__feature_settings() );
 
-    $ntt_f5e_array = array(
+    $feature_array = array(
         ntt__kid_ntt__features__info()['slug'],
     );
 
-    if ( strpos_array( $snaps_feature_settings, $ntt_f5e_array ) ) {
+    if ( strpos_array( $snaps_feature_settings, $feature_array ) ) {
         return true;
     }
 }
@@ -59,23 +66,27 @@ function ntt__kid_ntt__feature__scroll_y__snaps_settings_validation() {
  */
 function ntt__kid_ntt__feature__scroll_y__styles_scripts() {
 
-    $slug = ntt__kid_ntt__features__info()['slug'];
-    $prefixed_name = 'ntt--kid-ntt--feature--'. $name;
-    $version = ntt__kid_ntt__features__info()['version'];
+    $feature_slug = ntt__kid_ntt__features__info()['slug'];
+    $feature_prefix = ntt__kid_ntt__features__info()['prefix'];
+    $feature_prefixed_name = $feature_prefix. $feature_slug;
+    $feature_version = ntt__kid_ntt__features__info()['version'];
     $theme_version = wp_get_theme()->get( 'Version' );
 
     if ( ntt__kid_ntt__feature__scroll_y__validation() || ntt__kid_ntt__feature__scroll_y__snaps_settings_validation() ) {
 
-        wp_enqueue_script( $prefixed_name. '--script', get_stylesheet_directory_uri(). '/includes/features/'. $slug. '/main.js', array(), $version. '-'. $theme_version, true );
+        wp_enqueue_script( $feature_prefixed_name. '--script', get_stylesheet_directory_uri(). '/includes/features/'. $feature_slug. '/main.js', array(), $feature_version. '-'. $theme_version, true );
     }
 }
 add_action( 'wp_enqueue_scripts', 'ntt__kid_ntt__feature__scroll_y__styles_scripts', 0 );
 
 // View CSS
 function ntt__kid_ntt__feature__scroll_y__view__css( $classes ) {
+    $feature_slug = ntt__kid_ntt__features__info()['slug'];
+    $feature_prefix = ntt__kid_ntt__features__info()['prefix'];
+    $feature_prefixed_name = $feature_prefix. $feature_slug;
     
     if ( ( is_singular() && ntt__kid_ntt__feature__scroll_y__validation() ) || ntt__kid_ntt__feature__scroll_y__snaps_settings_validation() ) {
-        $classes[] = esc_attr( $GLOBALS['ntt__gvar__kid_ntt__feature__scroll_y__prefixed_name'] );
+        $classes[] = esc_attr( $feature_prefixed_name );
     }
     
     return $classes;
@@ -84,9 +95,12 @@ add_filter( 'ntt__wp_filter__view_css', 'ntt__kid_ntt__feature__scroll_y__view__
 
 // Entry CSS
 function ntt__kid_ntt__feature__scroll_y__entry__css( $classes ) {
+    $feature_slug = ntt__kid_ntt__features__info()['slug'];
+    $feature_prefix = ntt__kid_ntt__features__info()['prefix'];
+    $feature_prefixed_name = $feature_prefix. $feature_slug;
     
     if ( is_singular() && ntt__kid_ntt__feature__scroll_y__validation() ) {
-        $classes[] = esc_attr( $GLOBALS['ntt__gvar__kid_ntt__feature__scroll_y__prefixed_name']. '--entry' );
+        $classes[] = esc_attr( $feature_prefixed_name. '--entry' );
     }
     
     return $classes;
--- includes/snaps/hexetidine/functions.php
+++ includes/snaps/hexetidine/functions.php
@@ -90,3 +90,14 @@ function ntt__kid_ntt__snaps__hexetidine_ntt__function__icons() {
 
     return $icons;
 }
+
+function ntt__kid_ntt__snaps__hexetidine_ntt__function__exclude_pages_from_search( $query ) {
+    
+    if ( $query->is_main_query() && is_search() ) {
+        $query->set( 'post_type', 'page' );
+        $query->set( 'category__not_in', '7' );
+    }
+    
+    return $query;
+}
+add_filter( 'pre_get_posts','ntt__kid_ntt__snaps__hexetidine_ntt__function__exclude_pages_from_search' );
