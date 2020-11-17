a89a80c9c748a7bae73315f120835b2181969afc
Brian Dys Sahagun <briansahagun@gmail.com>
2020-11-17 09:17:28 +0800

    Updates

--- includes/tags/content-none.php
+++ includes/tags/content-none.php
@@ -1,8 +1,15 @@
 <?php
 if ( ! function_exists ( 'ntt__kid_ntt__tag__content_none' ) ) {
-    function ntt__kid_ntt__tag__content_none() {
+    function ntt__kid_ntt__tag__content_none( $subject ='' ) {
+
+        if ( $subject !== '' ) {
+            $phrase = '<span class="ntt--empty-content-subject--txt">'. __( ' '. 'for'. ' '.  $subject, 'ntt' ). '</span>';
+        } else {
+            $phrase = '';
+        }
+
         $mu = '<div class="ntt--empty-content-note ntt--note ntt--cp" data-name="Empty Content Note">';
-            $mu .= '<p>'. esc_html__( 'Content not found.', 'ntt' ). '</p>';
+            $mu .= '<p>'. '<span class="ntt--empty-content-predicate--txt">'. esc_html__( 'Content not found', 'ntt' ). '</span>'. $phrase. '.'. '</p>';
         $mu .= '</div>';
         
         echo $mu;
